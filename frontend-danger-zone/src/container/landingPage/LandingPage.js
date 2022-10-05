715c7e22543122b80471998b3354153f0ef9c1ee
ljanssen <linus.janssen@prodyna.com>
2022-10-05 15:14:49 +0200

    added button

--- frontend-danger-zone/src/container/landingPage/LandingPage.js
+++ frontend-danger-zone/src/container/landingPage/LandingPage.js
@@ -3,6 +3,7 @@ import {Grid, Typography} from "@mui/material";
 
 import dangerZone from "../../assets/danger-zone.png";
 import NavBar from "../../components/NavBar/NavBar";
+import Button from "@mui/material/Button";
 
 export function LandingPage () {
     return(
@@ -10,7 +11,7 @@ export function LandingPage () {
             <Grid item xxs={12}>
                 <img src={dangerZone} alt="Logo" style={{maxWidth: "100%"}}/>
             </Grid>
-            <Grid item xxs={12}>
+            <Grid item xxs={12} style={{marginBottom: "10%"}}>
                 <Typography variant="body1">
                     Adrenalin ist dein zweiter Vorname?
 
@@ -21,6 +22,13 @@ export function LandingPage () {
                     Willkommen bei Danger Zone - Abenteuer mit finalem Kick!
                 </Typography>
             </Grid>
+            <Grid item xxs={3}/>
+            <Grid item xxs={6}>
+                <Button variant="contained">
+                    Find your adventures
+                </Button>
+            </Grid>
+            <Grid item xxs={3}/>
 
         </Grid>
     )
